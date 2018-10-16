28b1ead4089be614229b6b701700e4890e37b672
Rehan-Sattar <rehansattar117gmail.com>
2018-10-16 22:52:55 +0500

    Doctor Modal

--- server/modals/doctorModal.js
+++ server/modals/doctorModal.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+const DoctorSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    gender: String
+});
+
+module.exports = mongoose.model('Doctor', DoctorSchema);
