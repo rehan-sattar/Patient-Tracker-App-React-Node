b722d133e868f2318b0e4bc64bb7de9db2e0e855
Rehan-Sattar <rehansattar117gmail.com>
2018-10-16 22:57:12 +0500

    store added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from "react-redux";
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import store from "./store/store";
+import './index.css';
+import "./firebase";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
